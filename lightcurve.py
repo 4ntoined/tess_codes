c6e7019b80defe964203a8fabb3de856becac82d
Antoine <antoine.washington98@gmail.com>
2023-06-11 15:05:35 -0400

    stay on with me

--- lightcurve.py
+++ lightcurve.py
@@ -11,16 +11,21 @@ def filter99(data, date=np.array([]), mask=-99.999):
         return (dataxx, datexx)
     else:
         return dataxx
-def plot_lc(xdat,ydat,yerr,xlims='',ylims='',save=''):
-    fig,ax = plt.subplots()
+def plot_lc(xdat,ydat,yerr,xlims='',ylims='',save='',plot_delta=''):
+    global delx, delt,autokm
+    if plot_delta:  fig,(az,ax) = plt.subplots(2,1,sharex=True,height_ratios=[1,4])
+    else:           fig,ax = plt.subplots()
     fig.tight_layout()
     fig.set_size_inches(42,6)
     fig.dpi=100
     #
     xdat = xdat - 2459936.89779
+    delx = delx - 2459936.89779
+    delt = delt / autokm
     #
     ax.scatter(xdat,ydat,s=4.)
     ax.errorbar( xdat, ydat, yerr=yerr, ls='none', lw=1. )
+    if plot_delta: az.plot(delx, delt, lw=1.,color='k')
     #
     if ylims:   ax.set_ylim(ylims[0],ylims[1])
     if xlims:
@@ -28,19 +33,31 @@ def plot_lc(xdat,ydat,yerr,xlims='',ylims='',save=''):
         ax.set_xticks(np.arange(4,25,1))
     ax.set_xlabel('Days from observation start')
     ax.set_ylabel('TESS Magnitude')
+    ax.grid(axis='both')
+    if plot_delta:
+        az.set_ylabel('TESS-Didymos distance [AU]')
+        az.grid(axis='y')
     #
-    plt.gca().invert_yaxis()
+    ax.invert_yaxis()
     #
-    if save: plt.savefig(save,dpi=200,bbox_inches='tight')
+    if save: plt.savefig(save,dpi=300,bbox_inches='tight')
     plt.show()
     return
-
+autokm = 1.496e8 #km in an au
 ipath = '/run/user/1000/gvfs/sftp:host=copernicus.astro.umd.edu,user=antoine/'
         #'Volumes/TESS_05/AW_Data/Sector_60/S60_cam1_ccd4/Didymos/Didymos_comphot/comphot_mags.dat'
 path1='Volumes/TESS_05/AW_Data/Sector_60/S60_cam1_ccd4/Didymos/Didymos_comphot/comphot_mags.dat'
 path2 = 'Users/antoine/tess_output/Data/Sector_60/S60_cam1_ccd4/Didymos_files/Didymos/Didymos_comphot/comphot_mags.dat'
-idata = np.loadtxt(ipath+path2,dtype=float,skiprows=2,usecols=(1,4,5,6,7,8,9,10,11,12,13,14,15,16,17))
+path3 = '/home/antoine/Desktop/codespace_tess/comphot_mags.dat'
+jpath = '/home/antoine/Desktop/codespace_tess/didy_delta_eph.txt'
+#idata = np.loadtxt(ipath+path2,dtype=float,skiprows=2,usecols=(1,4,5,6,7,8,9,10,11,12,13,14,15,16,17))
+idata = np.loadtxt(path3,dtype=float,skiprows=2,usecols=(1,4,5,6,7,8,9,10,11,12,13,14,15,16,17))
+idata2 = np.loadtxt(jpath,dtype=float,usecols=(2,11))
 #print(idata[:,5])
+delt = idata2[:,1].copy()
+delx= idata2[:,0].copy()
+#plt.plot( idata2[:,0], idata2[:,1] )
+#plt.show()
 
 #idata25 = np.where( idata[:,5] == -99.999, np.nan, idata[:,5] )
 #idate25 = np.where( idata[:,5] == -99.999, np.nan, idata[:,0] )
@@ -50,7 +67,7 @@ ierr25 = filter99(idata[:,6],mask=-9.999)
 #idate25-=2459936.89779
 #plot_lc(idate25,idata25,ierr25,xlims=(2459940.,2459950.2),ylims=(12.5,13.5),save='s1.png')
 #plot_lc(idate25,idata25,ierr25,xlims=(2459954.8,2459961.5),ylims=(12.75,14.25),save='s2.png')
-plot_lc(idate25,idata25,ierr25,xlims=(2459940.,2459961.5),ylims=(12.5,14.25),save='s4.png')
+plot_lc(idate25,idata25,ierr25,xlims=(2459940.,2459961.5),ylims=(12.5,14.25),plot_delta=True,save='s8.png')
 
 #fig,ax = plt.subplots()
 #fig.figsize = (8,6)
